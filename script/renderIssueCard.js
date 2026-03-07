57d643daf4dfa543616adb1b31f61873535f3ccc
mdsiamkazi <mdsiamkazibd@gmail.com>
2026-03-07 06:34:27 +0600

    tab interactivity implentation

--- script/renderIssueCard.js
+++ script/renderIssueCard.js
@@ -1,14 +1,46 @@
+let issueContainer = document.getElementById("issue-card-container");
+let issueQ = document.getElementById("issue-quantity");
+let allData ;
+const renderLabels = (labels) => {
+  let html = "";
+  console.log(labels);
+  labels.forEach((label) => {
+    
+    let x;
 
-let issueContainer = document.getElementById('issue-card-container');
-let issueQ = document.getElementById('issue-quantity');
+    if (label === "documentation") {
+      x = `<div class="rounded-full text-[12px] font-medium bg-gray-300 w-fit ">
+            <i class="fa-solid fa-file"></i> ${label.toUpperCase()}
+         </div>`;
+    } else if (label === "enhancement") {
+      x = `<div class="rounded-full text-[12px] font-medium bg-blue-300 w-fit ">
+            <i class="fa-solid fa-arrow-turn-up"></i> ${label.toUpperCase()}
+         </div>`;
+    } else if (label === "bug") {
+      x = `<div class="rounded-full text-[12px] font-medium bg-red-300 w-fit ">
+            <i class="fa-solid fa-bug"></i> ${label.toUpperCase()}
+         </div>`;
+    } else if (label === "help wanted") {
+      x = `<div class="rounded-full text-[12px] font-medium bg-yellow-500 w-fit ">
+            <i class="fa-solid fa-hands-helping"></i> ${label.toUpperCase()}
+         </div>`;
+    } else if (label === "good first issue") {
+      x = `<div class="rounded-full text-[12px] font-medium bg-green-300 w-fit ">
+            <i class="fa-solid fa-heart"></i> ${label.toUpperCase()}
+         </div>`;
+    }
+    html += x;
+  });
+  return html;
+};
 
-const renderIssueCard = (issues)=>{
-    let html = "";
-    issues.forEach(issue => {
-        let x = `<div class="issue-card shadow rounded-2xl border-t-5  ${issue.status === 'open' ? 'border-t-green-500' : 'border-t-blue-700'}">
+const renderIssueCard = (issues) => {
+  let html = "";
+  issues.forEach((issue) => {
+    let x = `<div class="issue-card shadow rounded-2xl border-t-5  ${issue.status === "open" ? "border-t-green-600" : "border-t-blue-700"}">
                 <div class="ic-first border-b-1 border-b-[#ddd] w-full p-4">
                     <div class="ic-heading w-full flex justify-between items-center mb-3">
-                        <img src="./assets/${(issue.status === 'open'?'Open-Status.png':'c.png')}" class="w-6 h-6">
+                        <img src="./assets/${issue.status === "open" ? "Open-Status.png" : "c.png"}" class="w-6 h-6">
                         <div class="px-6 py-2 bg-[#FEECEC] rounded-full text-[14px] text-[#EF4444]">${issue.priority.toUpperCase()}</div>
                     </div>
 
@@ -18,8 +50,8 @@ const renderIssueCard = (issues)=>{
                         <p class="line-clamp-2 text-xs  text-[#64748B] mt-2">${issue.description}</p></div>
 
 
-                        <div class="label-container mt-3 flex">
-                            <div class="rounded-full text-[14px] font-medium bg-green-500 w-fit p-1.5"><i class="fa-solid fa-arrow-turn-up"></i>enhancement</div>
+                        <div class="label-container mt-3 gap-[1px] flex flex-wrap">
+                            ${renderLabels(issue.labels)}
 
 <!-- enhancement 
 
@@ -27,26 +59,28 @@ documentation
 
 good first issue
 
-help wanted -->
+help wanted help wanted -->
                         </div>
                     </div>
                 </div>
                 <div class="ic-last p-4 text-xs text-[#64748B]">
-                    <p class="mb-2">#${issue.id} ${issue.assignee != ''?'by '+issue.assignee:''}</p>
+                    <p class="mb-2">#${issue.id} ${issue.assignee != "" ? "by " + issue.assignee : ""}</p>
                     <p>1/33/2222</p>
                 </div>
             </div>`;
-            html += x;
-    });
-    issueContainer.innerHTML = html
-    renderIssueQ(issues.length);
-}
+    html += x;
+  });
+  issueContainer.innerHTML = html;
+  renderIssueQ(issues.length);
+};
 
-const firstHtml = async()=>{
-    let response = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
-    let responseData = await response.json();
-    let data =await responseData.data;
-    renderIssueCard(data);
-
-}
-firstHtml()
+const firstHtml = async () => {
+  let response = await fetch(
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+  );
+  let responseData = await response.json();
+  let data = await responseData.data;
+  allData = data;
+  renderIssueCard(data);
+};
+firstHtml();
---
+++ script/tab.js
@@ -0,0 +1,21 @@
+document.getElementById('tab').addEventListener('click' ,(evt) => {
+    
+    if(evt.target.classList.contains('btn')){
+        document.querySelectorAll('.btn').forEach(btn => {
+        btn.classList.remove('btn-primary')
+    })
+        if(evt.target.id === 'all'){
+            renderIssueCard(allData);
+            evt.target.classList.add('btn-primary')
+        }
+        else if(evt.target.id === 'open'){
+            let openData = allData.filter(data => data.status === 'open');
+            evt.target.classList.add('btn-primary')
+            renderIssueCard(openData)
+        }else if(evt.target.id === 'closed'){
+            let closedData = allData.filter(data => data.status === 'closed');
+            evt.target.classList.add('btn-primary')
+            renderIssueCard(closedData);
+        }
+    }
+})
