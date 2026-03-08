e9d06a3b3ee951181ecf21fac00c95a224f7afaf
Md Siam kazi <122417782+md-siam-kazi-dev@users.noreply.github.com>
2026-03-08 08:41:00 +0600

    Update mac.js
    bug fix

--- script/mac.js
+++ script/mac.js
@@ -76,7 +76,7 @@ help wanted help wanted -->
     //sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4
     html = `<div class="w-full h-50 flex sm:col-span-2 lg:col-span-3 xl:col-span-4 justify-center items-center font-bold text-2xl">No Issue Found!</div>`
   }
-  
+  renderLoading(false);
   issueContainer.innerHTML = html;
   renderIssueQ(issues.length);
 };
